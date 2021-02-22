d2976bc4f924d37a1d298bd7334970884a5e7965
mungai-joel <mungaijoel123@gmail.com>
2021-02-22 14:24:53 +0300

    innit file update

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,10 @@
 from flask import Flask
 from flask-bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+def create app(config_name):
+    app = Flask(__name__)
+
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])

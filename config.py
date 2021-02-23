780f551a485530a2aef6e7b58e7b740bed572afd
mungai-joel <mungaijoel123@gmail.com>
2021-02-23 11:43:20 +0300

    added devconfig

--- config.py
+++ config.py
@@ -10,6 +10,16 @@ class Config():
     NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
     SECRET_KEY=os.environ.get('SECRET_KEY')
 
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
 class prodConfig(Config):
     '''
     production configuration of child class

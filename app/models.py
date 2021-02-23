83df0cbc424dae5c9d03247a4d04cbd46bf518ef
mungai-joel <mungaijoel123@gmail.com>
2021-02-23 08:46:57 +0300

    added source class in models

--- app/models.py
+++ app/models.py
@@ -2,4 +2,10 @@ class Source:
     '''
     source class to define news sources
     '''
-    
+    def __init__ (self,id,name,description,url,category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+

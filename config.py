2453e2610dddd1f8aaa4405b801ccb93c9304ffc
mungai-joel <mungaijoel123@gmail.com>
2021-02-23 10:52:06 +0300

    added api key

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ class Config():
     General configuration of the parent class
 
     '''
+    NEWS_API_BASE_URL = "https://newsapi.org/v2/{}?apiKey={}"
+    ARTICLE_API_BASE_URL= "https://newsapi.org/v2/everything?sources={}&apiKey={}"
+    NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
+    SECRET_KEY=os.environ.get('SECRET_KEY')
 
 class prodConfig(Config):
     '''
@@ -19,3 +23,6 @@ config_options = {
     'development':DevConfig,
     'production':prodConfig
 }
+
+
+"apikey = 03fb9adcb9bf415ebfa36d87fedc0653"
